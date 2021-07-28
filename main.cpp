3ae527025830485a0381fceda5c488df2e490dbc
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-07-28 09:40:46 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    
+    int sum;
+    int sub;
+    int prod;
+    int divid;
+    
+    cout << "Enter the first number! \n";
+    cin >> a;
+    
+    cout << "Enter the second number! \n";
+    cin >> b;
+    
+    sum=a+b;
+    cout << "The sum of those numbers is " <<sum;
+    return 0;
+    
+    cout << "Enter the first number for subtraction! \n";
+    cin >> c;
+    
+    cout << "Enter the second number for subtraction! \n";
+    cin >> d;
+    
+    sum=c-d;
+    cout << "The value of those numbers is " <<sum;
+    return 0;
+    
+    cout << "Enter the first number for multiplication! \n";
+    cin >> e;
+    
+    cout << "Enter the second number for multiplication! \n";
+    cin >> f;
+    
+    sum=e*f;
+    cout << "The product of those numbers is " <<sum;
+    return 0;
+}
